2bb554e3712189470b76ff9bb87322393a6b0dbc
oriya2234567 <oriyarabani15@gmail.com>
2025-06-25 01:50:24 +0300

    class dog and some funcshion

--- chakgit/chack.py
+++ chakgit/chack.py
@@ -1,4 +1,7 @@
-x =2
-x =8
-print(x)
-print(x+1)
+x =4
+
+try:
+    ans = int(input("enter some number: "))
+    print(ans)
+except:
+    print("invalid input error")
---
+++ targulnogit/targulnogit2/one.py
@@ -0,0 +1,39 @@
+
+class Dog():
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self,value):
+        if type(value) == int:
+            self._age  = value
+
+    def __str__(self):
+        return f" {self._name}  is {self._age} years old"
+    
+    def cleandog(self):
+        return f"the price for cleaning {self._name} is 20 dollars  "
+
+rax = Dog("rax " ,3)
+print(rax.name)
+rax.name = "maaa"
+rax.age =13
+print(rax.age)
+rax.name = "flafi"
+print(rax)
+print(rax.cleandog())
+
+
